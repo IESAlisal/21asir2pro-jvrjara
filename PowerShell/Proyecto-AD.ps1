e3e56fdc263725e6e6de5d9f09f5a995f19e425e
jvrjara <jjaraa01@educantabria.es>
2022-03-28 14:31:37 +0200

    Update Proyecto-AD.ps1

--- PowerShell/Proyecto-AD.ps1
+++ PowerShell/Proyecto-AD.ps1
@@ -17,9 +17,30 @@ $contraseña = "Usuario@1"
 
 $empresa = @('Planta0','Planta1','Planta2','Planta3')
 
-FOR ($i=0;$i -le $empresa.Length -1;$i++){
+for ($i=0;$i -le $empresa.Length -1;$i++){
 
     $empre = $empresa[$i]
-    New-ADOrganizarionalUnit -Name $empre -Path "OU-Usuarios.$ruta"
-    New-ADGroup -GroupCategory Security -GroupScope Global -Name ""
+    New-ADOrganizarionalUnit -Name $empre -Path "OU=Usuarios.$ruta"
+    New-ADGroup -GroupCategory Security -GroupScope Global -Name $empre -Path "OU=$empre,OU=Usuarios.$ruta"
+
+    for($n=1;$n -le 10;$n++){
+        $CrearUser = $empre=("{O:D2}" -f $n)
+        New-ADUser -Name $CrearUser -AccountPassword(ConvertTo-SecureString -AsPlainText $contraseña -Force) -Enabled $true -ChangePasswordAtLogon 1 -Path "OU=$empre,OU=Usuario.$ruta"
+        Add-ADGroupMember -Identity $empre -Members $CrearUser
+    }
+}
+
+#Maquinas de cada usuario
+
+$Maquina = @('A01','A02','A03','A04','A05','A06')
+
+for($i=0;$i -le $Maquina.Length -1;$i++){
+    $NEW = $Maquina[$i]
+    $OUMaquinas = New-ADOrganizarionalUnit -Name $NEW -Path "OU=Maquinas.Ruta"
+
+    for(for($x=1;$x -le 10;$x++){
+        $CrearO = $NEW=("{O:D2}" -f $n)
+        New-ADComputer -Name $CrearO -Name $CrearO -Path "OU=$NEW,OU=maquinas.$ruta"
+        
+    }
 }
