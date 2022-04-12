ecb4796f3129e45ea0eb2166506ad32b8f453e98
jvrjara <jjaraa01@educantabria.es>
2022-04-12 11:07:20 +0200

    Update Proyecto-AD.ps1

--- PowerShell/Proyecto-AD.ps1
+++ PowerShell/Proyecto-AD.ps1
@@ -4,6 +4,8 @@
 #######                28/03/2022 - 12/04/2022                #######
 #####################################################################
 
+#Opcion 1
+
 $ruta = (Get-ADDomain).DistinguishedName
 
 #Se debe realizar un Update porque por defecto un servidor no tiene instalado los cmdlet de AD
@@ -13,22 +15,24 @@ $Usuarios = New-ADOrganizationalUnit -Name usuarios -Path $ruta
 $maquinas = New-ADOrganizationalUnit -Name maquinas -Path $ruta
 $Usuarios ; $maquinas
 
-#Todos los usuario y maquinas para la empresa
+#Todos los usuarios y la unidad organizativa para la empresa
 #Los nombres pueden cambiar
 
-#Definir una variable para la contraseña de cada usuario
 $grupos = @('NAFT-Planta0','NAFT-Planta1','NAFT-Planta2','NAFT-Planta3')
 
 for($i=0;$i -le $grupos.Length -1 ;$i++){
 
     $grupo = $grupos[$i]
+    #Crea una unidad organizativa que estaran todos los usuarios
      $ejecutar = New-ADOrganizationalUnit -Name $grupo -Path "OU=usuarios,$ruta" 
      New-ADGroup -GroupCategory Security -GroupScope Global -Name $grupo -Path "OU=$grupo,OU=usuarios,$ruta"
   
      for($n=1; $n -le 10; $n++){
      
         $crearUser =$grupo+"-"+("{0:D2}"-f$n)
+        #Definir variable contraseña para todos los usuarios al comienzo 
         $contrasena = "Usuario@1"
+        #El usuario al iniciar sesion por primera vez debe cambiar la contraseña
         New-ADUser -Name $crearUser -AccountPassword(ConvertTo-SecureString -AsPlainText $contrasena -Force) -Enabled $true -ChangePasswordAtLogon 1 -Path "OU=$grupo,OU=usuarios,$ruta"
         Add-ADGroupMember -Identity $grupo -Members $crearUser
         }
@@ -51,3 +55,5 @@ for($i=0;$i -le $OUmas.Length -1 ;$i++){
         }
 
 }
+
+#Opcion 2
