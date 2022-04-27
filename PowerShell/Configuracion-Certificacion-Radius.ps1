189c4f3b541d27ad0fc14a99fc9946695ca44437
jvrjara <jjaraa01@educantabria.es>
2022-04-27 15:26:30 +0200

    Create Configuracion-Certificacion-Radius.ps1

---
+++ PowerShell/Configuracion-Certificacion-Radius.ps1
@@ -0,0 +1,29 @@
+﻿##Visualizar 
+Get-WindowsFeature -Name NPAS
+
+##
+Install-WindowsFeature -Name NPAS
+
+##
+Get-WindowsFeature -Name NPAS
+
+##
+Get-Command -Name *NPS*
+
+##
+## Export-NpsConfiguration - Exports NPS settings.
+## Get-NpsRadiusClient - Gets RADIUS clients.
+## Get-NpsRemediationServer - Retrieves a list of remediation servers from a remediation server group.
+## Get-NpsRemediationServerGroup - Retrieves all remediation server groups from a Network Policy Server.
+## Get-NpsSharedSecretTemplate - Returns a list of available shared secret templates.
+## Import-NpsConfiguration - Imports NPS settings.
+## New-NpsRadiusClient - Creates a RADIUS client.
+## New-NpsRemediationServer - Creates a remediation server.
+## New-NpsRemediationServerGroup - Creates a remediation server group.
+## Remove-NpsRadiusClient -Removes a RADIUS client.
+## Remove-NpsRemediationServer - Removes a remediation server from a remediation server group.
+## Remove-NpsRemediationServerGroup - Removes a remediation server group from an NPS.
+## Set-NpsRadiusClient - Specifies configuration settings for a RADIUS client.
+
+
+
